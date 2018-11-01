2753aaeacee14f4b7a641246dd51953d74032f29
MUHAMMAD NOUMAN ZAFAR - STUDENT <G00353137@gmit.ie>
2018-11-01 13:58:09 +0000

    get method done

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -10,70 +10,57 @@ mongoose.connect(mongoDB);
 
 const Schema = mongoose.Schema;
 const PostSchema = new Schema({
-    title:String,
-    content:String
+    title: String,
+    content: String
 });
 
 //database 
-const PostModel = mongoose.model('postA',PostSchema);
+const PostModel = mongoose.model('postA', PostSchema);
 
 
 //Here we are configuring express to use body-parser as middle-ware. 
-app.use(bodyParser.urlencoded({ extended: false })); 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept");
+        "Origin, X-Requested-With, Content-Type, Accept");
     next();
-    });
-    
-app.post('/name', function(req, res){
+});
+
+app.post('/name', function (req, res) {
     res.send("Hello you sent " +
-    req.body.firstname + " " +
-    req.body.lastname);
+        req.body.firstname + " " +
+        req.body.lastname);
 })
 
 app.get('/', function (req, res) {
-   res.send('Hello from Express');
+    res.send('Hello from Express');
 })
 
-app.post('/api/posts', function(req, res){
+app.post('/api/posts', function (req, res) {
     console.log("post successful");
     console.log(req.body.title);
     console.log(req.body.content);
 
-    
+
     PostModel.create({
-        title:req.body.title,
-        content:req.body.content
-     })
+        title: req.body.title,
+        content: req.body.content
+    })
 })
 
-app.get('/api/posts', function(req, res){
-
-    const posts = 
-    [
-        { 
-            "id": "fadf12421l", 
-            "title": "First server-side post", 
-            "content": "This is coming from the server" 
-        }, 
-        { 
-            "id": "ksajflaj132", 
-            "title": "Second server-side post", 
-            "content": "This is coming from the server!" 
-        }
-    ];
-
-    res.status(200).json({posts:posts})
+app.get('/api/posts', function (req, res) {
+    PostModel.find(function (err, data) {
+        res.json(data);
+    })
 })
 
 
 var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("Example app listening at http://%s:%s", host, port)
 })
